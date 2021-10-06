1ebed54ad0648e21581b007962be9c511cbefcb0
Sania-Iftikhar <saniaiftikhar27@gmail.com>
2021-10-07 01:29:30 +0500

    projects added

---
+++ 14-cocktails/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est, tenetur eaque! Cupiditate pariatur nisi excepturi? Accusamus vel necessitatibus non ex eveniet dolore assumenda ad facilis. Suscipit cumque aliquid nobis harum?
+      </p>
+    </section>
+  )
+}
+
+export default About
